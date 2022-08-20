d5964d7834525f80dc0b12e7d64207c3668d6c95
Moon2 <gunyunghi@gmail.com>
2022-08-20 19:10:54 +0900

    뚜방뚜방

---
+++ 2. python/2. list,dictionary.py
@@ -0,0 +1,13 @@
+# list
+중고차 = ['K5', 'white', '5000']
+print(중고차)
+중고차[1] = 'black'
+
+# dictionary
+중고차2 = {'brand' : 'BMW', 'model' : '520d'}
+print(중고차[1])
+print(중고차2['model'])
+중고차2['model'] = "740d"
+print(중고차2)
+
+property
---
+++ 2. python/4. for.py
@@ -0,0 +1,11 @@
+# for 반복문
+# 1. 노가다 줄일 때 씀
+for i in range(0,2) :
+    print('벤츠 있어용')
+print('K5 있어용')
+print('K5 있어용')
+
+# 2. list에서 자료 하나씩 뽑을 때
+중고차들 = ['K5', 'BMW', 'SEX']
+for i in 중고차들 :
+    print(i)
